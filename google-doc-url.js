a7d7a28ffc3ebaf7000081daadeeb132181184e3
afarboleda <aarboleda84@gmail.com>
2017-02-22 14:00:22 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yk2i-2K0329mrlnmek83hzcFrju-vUwGBr2KwxCeTBk/pubhtml';
